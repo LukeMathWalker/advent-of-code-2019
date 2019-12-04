8416ef9a186bbfb3b2b95222897efdba116a1b76
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-12-04 09:13:44 +0000

    First half

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,10 @@
 use std::io::BufRead;
 use std::str::FromStr;
+use std::collections::HashSet;
 
+type GridPoint = (i64, i64);
+
+#[derive(Clone)]
 enum Direction {
     Up,
     Down,
@@ -15,14 +19,22 @@ impl Direction {
             "D" => Direction::Down,
             "R" => Direction::Right,
             "L" => Direction::Left,
-            s => {
-                println!("{}", s);
-                panic!("Unknown direction - failed to parse.")
-            },
+            _ => panic!("Unknown direction - failed to parse."),
+        }
+    }
+
+    fn move_unit(&self) -> Box<dyn Fn(GridPoint) -> GridPoint>
+    {
+        match self {
+            Direction::Up => Box::new(|p: GridPoint| (p.0 + 1, p.1)),
+            Direction::Down => Box::new(|p: GridPoint| (p.0 - 1, p.1)),
+            Direction::Right => Box::new(|p: GridPoint| (p.0, p.1 + 1)),
+            Direction::Left => Box::new(|p: GridPoint| (p.0, p.1 - 1)),
         }
     }
 }
 
+#[derive(Clone)]
 struct Movement {
     direction: Direction,
     distance: u64,
@@ -52,7 +64,28 @@ fn read_input(path: &str) -> Vec<Vec<Movement>> {
         .collect()
 }
 
+fn get_grid_points(wire: &[Movement]) -> HashSet<GridPoint> {
+    let mut current_position = (0, 0);
+
+    let mut grid_points = HashSet::new();
+
+    for movement in wire {
+        let step_function = movement.direction.move_unit();
+        for _ in 0..movement.distance {
+            current_position = step_function(current_position);
+            grid_points.insert(current_position.clone());
+        }
+    }
+    grid_points
+}
+
 fn main() {
     let movements = read_input("input.txt");
     assert_eq!(movements.len(), 2);
+    let (first_wire, second_wire) = (movements[0].clone(), movements[1].clone());
+    let first_grid_points = get_grid_points(&first_wire);
+    let second_grid_points = get_grid_points(&second_wire);
+    let common_points = first_grid_points.intersection(&second_grid_points);
+    let closest_point = common_points.into_iter().min_by(|p, q| (p.0.abs() + p.1.abs()).cmp(&(q.0.abs() + q.1.abs()))).unwrap();
+    println!("Closest point: {:?}", closest_point);
 }
