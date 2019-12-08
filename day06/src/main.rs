7f3ac0ada54eadc6a03fa4cb8ab02a824076f226
LukeMathWalker <rust@lpalmieri.com>
2019-12-08 16:16:33 +0000

    Part 1, actually working

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -4,6 +4,11 @@ use std::io::BufRead;
 use petgraph::algo::is_cyclic_directed;
 
 fn read_input(path: &str) -> Vec<(String, String)> {
+    fn parse(l: &str) -> (String, String) {
+        let parts: Vec<_> = l.split(')').collect();
+        (parts[0].into(), parts[1].into())
+    }
+
     let file = std::fs::File::open(path).expect("Failed to open file.");
     let reader = std::io::BufReader::new(file);
 
@@ -11,31 +16,23 @@ fn read_input(path: &str) -> Vec<(String, String)> {
         .lines()
         .map(|l| {
             let line = l.expect("Failed to read line");
-            let center = line[0..3].to_string();
-            let orbiting = line[4..6].to_string();
-            (center, orbiting)
+            parse(&line)
         })
         .collect()
 }
 
-fn count_orbits(graph: &DiGraphMap<&str, ()>) -> usize {
-    let n_direct_orbits = graph.edge_count();
-    let n_indirect_orbits = count_indirect_orbits(graph, "COM");
-    n_direct_orbits + n_indirect_orbits
-}
-
-fn count_indirect_orbits<'a>(graph: &DiGraphMap<&'a str, ()>, root_node: &'a str) -> usize {
+fn count_orbits<'a>(graph: &DiGraphMap<&'a str, ()>, root_node: &'a str) -> usize {
     let mut acc = 0;
     for neighbour in graph.neighbors_directed(root_node, Direction::Outgoing) {
-        acc += _count_indirect_orbits(&graph, &neighbour, 0);
+        acc += _count_indirect_orbits(&graph, &neighbour, 1);
     }
     acc
 }
 
-fn _count_indirect_orbits<'a>(graph: &DiGraphMap<&'a str, ()>, node: &'a str, depth: usize) -> usize {
+fn _count_orbits<'a>(graph: &DiGraphMap<&'a str, ()>, node: &'a str, depth: usize) -> usize {
     let mut acc = depth;
     for neighbour in graph.neighbors_directed(node, Direction::Outgoing) {
-        acc += _count_indirect_orbits(&graph, &neighbour, depth + 1);
+        acc += _count_orbits(&graph, &neighbour, depth + 1);
     }
     acc
 }
@@ -46,14 +43,14 @@ fn main() {
     let graph: DiGraphMap<_, ()> = DiGraphMap::from_edges(edges);
 
     assert!(!is_cyclic_directed(&graph));
-    let n_orbits = count_orbits(&graph);
+    let n_orbits = count_orbits(&graph, "COM");
     println!("Total number of orbits: {:?}.", n_orbits);
 }
 
 #[cfg(test)]
 mod tests {
     use petgraph::graphmap::DiGraphMap;
-    use crate::count_orbits;
+    use crate::{count_orbits, parse};
 
     #[test]
     fn part_1_example() {
@@ -71,7 +68,7 @@ mod tests {
             ("K", "L"),
         ];
         let graph: DiGraphMap<_, ()> = DiGraphMap::from_edges(&edges);
-        let n_orbits = count_orbits(&graph);
+        let n_orbits = count_orbits(&graph, "COM");
         assert_eq!(n_orbits, 42);
     }
 }
