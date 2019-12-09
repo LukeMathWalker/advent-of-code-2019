5fbc7d26a346b442508891b6f4990788920e2522
LukeMathWalker <rust@lpalmieri.com>
2019-12-09 18:28:05 +0000

    Part 1 of day 8

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,3 +1,45 @@
+use std::alloc::handle_alloc_error;
+use std::str::FromStr;
+use ndarray::{Array2, Array3, Axis, ArrayView2};
+
+fn read_input(path: &str) -> Vec<u8> {
+    let input = std::fs::read_to_string(path).expect("Failed to read input");
+    let digits = input
+        .trim()
+        .chars()
+        .map(|s| u8::from_str(&s.to_string()).expect("Failed to parse digit"))
+        .collect();
+    digits
+}
+
+fn get_layers(width: usize, height: usize, digits: Vec<u8>) -> Array3<u8> {
+    let n_layers = digits.len() / (width * height);
+    Array3::from_shape_vec((n_layers, width, height), digits).unwrap()
+}
+
+fn checksum(layers: &Array3<u8>) -> u32 {
+    let layer = layers
+        .axis_iter(Axis(0))
+        .min_by_key(|l| count_digit(&l, 0))
+        .expect("Failed to find maximum layer");
+    count_digit(&layer, 1) * count_digit(&layer, 2)
+}
+
+fn count_digit(l: &ArrayView2<u8>, digit: u8) -> u32 {
+    l.iter().map(|&d| {
+        if d == digit {
+            1
+        } else {
+            0
+        }
+    }).sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let digits = read_input("input.txt");
+    let width = 25;
+    let height = 6;
+
+    let layers = get_layers(width, height, digits);
+    println!("Checksum: {:?}", checksum(&layers))
 }
